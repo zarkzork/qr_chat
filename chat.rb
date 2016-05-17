c6d41c092e8a81118d0d4f55a7069457e667e43f
Vladimir Terekhov <zarkzork@gmail.com>
2016-05-17 15:43:22 +0300

    add redis channels support

--- chat.rb
+++ chat.rb
@@ -3,23 +3,30 @@ require 'redis'
 require 'json'
 
 STDOUT.sync = true
+Thread.abort_on_exception = true
+
+chat = CGI.new.params.fetch('chat').first
+messages_key = "messages:#{chat}"
+subscription_key = "subscription:#{chat}"
 
 $redis = Redis.new(host: 'redis')
+emoji = ['🐶', '🐱', '🐭', '🐹', '🐰', '🐻', '🐼', '🐨', '🐯', '🦁', '🐮', '🐷', '🐽', '🐸', '🐙', '🐵', '🙈', '🙉', '🙊', '🐒', '🐔', '🐧', '🐦', '🐤', '🐣', '🐥', '🐺', '🐗', '🐴', '🦄', '🐝', '🐛', '🐌', '🐞', '🐜', '🕷', '🦂', '🦀', '🐍', '🐢', '🐠', '🐟', '🐡', '🐬', '🐳', '🐋', '🐊', '🐆', '🐅', '🐃', '🐂', '🐄', '🐪', '🐫', '🐘', '🐐', '🐏', '🐑', '🐎', '🐖', '🐀', '🐁', '🐓', '🦃', '🕊', '🐕', '🐩', '🐈', '🐇', '🐿', '😀', '😬', '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😉', '😊', '🙂', '🙃', '☺️', '😋', '😌', '😍', '😘', '😗', '😙', '😚', '😜', '😝', '😛', '🤑', '🤓', '😎', '🤗', '😏', '😶', '😐', '😑', '😒', '🙄', '🤔', '😳', '😞', '😟', '😠', '😡', '😔', '😕', '🙁', '☹️', '😣', '😖', '😫', '😩', '😤', '😮', '😱', '😨', '😰', '😯', '😦', '😧', '😢', '😥', '😪', '😓', '😭', '😵', '😲', '🤐', '😷', '🤒', '🤕', '😴', '💤', '💩', '😈', '👿', '👹', '👺', '💀', '👻', '👽', '🤖', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '🗿'].sample
 
-puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
-puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
-puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
-puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
-puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
-100.times do
-  puts({ body: 'hey everyone', message_type: 'message', author_emoji: '😚' }.to_json)
+$redis.lrange(messages_key, 0, 100).each do |message|
+  puts(message)
 end
 
-# puts $redis.inspect
-
-while foo = STDIN.readline.strip
-  puts eval(foo).inspect
+Thread.new do
+  Redis.new(host: 'redis').subscribe(subscription_key) do |subscription|
+    subscription.message do |_, message|
+      puts(message)
+    end
+  end
 end
 
-
-# $redis.rpush 'chat:room', { body: '', message_type '', author_emoji: '' }.to_json
+while body = STDIN.readline.strip
+  message = JSON.parse(body).merge(author_emoji: emoji, created_at: Time.now.to_i)
+  json = message.to_json
+  $redis.rpush(messages_key, json)
+  $redis.publish(subscription_key, json)
+end
